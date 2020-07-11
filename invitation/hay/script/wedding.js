aa70269e90bc13f4dd1116174585ae7d1be4d707
misolpark <1omo1@naver.com>
2020-07-12 01:03:13 +0900

    gg

--- invitation/hay/script/wedding.js
+++ invitation/hay/script/wedding.js
@@ -78,7 +78,7 @@ $(() => {
 
                 // 인포윈도우로 장소에 대한 설명을 표시합니다
                 var infowindow = new kakao.maps.InfoWindow({
-                    content: '<br /><a style="font-family:-webkit-body; font-weight:bold; color:#000;" href="http://mjcon.co.kr/?module=Html&action=SiteComp&sSubNo=2" target="_blank"> [MJ 컨벤션 5층 파티오홀]</a><br />'
+                    content: '<a style="display:inline-block; height:35px; text-align:center; font-size:0.85em; font-family:-webkit-body; font-weight:bold; color:#000;" href="http://mjcon.co.kr/?module=Html&action=SiteComp&sSubNo=2" target="_blank"> [MJ 컨벤션 5층 파티오홀]</a>'
                 });
                 infowindow.open(map, marker);
 
